d798466c137fe9aba7ad3b6705f08dc362d6dbad
Jitendra Kumar Mandal <btech10036.22@bitmesra.ac.in>
2025-08-30 20:34:02 +0530

    env_updated

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -6,6 +6,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? `${import.meta.env.VITE_BACKEND_URL}/api`
-      : "/api",
+      : `${import.meta.env.VITE_BACKEND_URL}/api`,
   withCredentials: true,
 });

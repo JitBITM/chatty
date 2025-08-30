fa3c1f3316366e0df79ab9fd7a9674fa23c1d268
Jitendra Kumar Mandal <btech10036.22@bitmesra.ac.in>
2025-08-30 20:18:44 +0530

    env_updated

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,11 @@
 import axios from "axios";
+// import dotenv from "dotenv";
 
+// dotenv.config();
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? `${import.meta.env.VITE_BACKEND_URL}/api`
+      : "/api",
   withCredentials: true,
 });
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -2,8 +2,11 @@ import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
+// import dotenv from "dotenv";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
+// dotenv.config();
+
+const BASE_URL = import.meta.env.MODE === "development" ? `${import.meta.env.VITE_BACKEND_URL}` : "/";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
